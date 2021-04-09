37bd2013d77e11da3813a7fd639063ce8a135ca9
Xavier <xavier@adaltas.com>
2021-04-09 17:11:18 +0200

    Add project

---
+++ src/main/java/com/adaltas/examples/App.java
@@ -0,0 +1,13 @@
+package com.adaltas.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
